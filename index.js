ef50bc5ac297a6514929838928102e558a438e2e
Demetrio Barrozo Dela Rosa Jr <45243271+demy2014@users.noreply.github.com>
2023-11-09 13:09:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,18 +5,19 @@ const port = process.env.PORT || 8080;
 // Route for the root URL
 app.get('/', (req, res) => {
   res.status(200).send('Welcome to the CI Server!. Testing the CI-Dev pipeline.');
+  res.sendFile('index.html', { root: __dirname });
 });
-
 
 app.get('/hello', (req, res) => {
   res.status(200).send('Hello Oklaa Dev Teams, there! This is a friendly greeting from the CI Dev-server.');
+  res.sendFile('index.html', { root: __dirname });
 });
 
 
 app.get('/about', (req, res) => {
   res.status(200).json({
     message: 'About CI-Dev Server',
-    description: 'This is a simple Express.js server for demonstration purposes.',
+    description: 'This is a simple Express.js CI Dev-server for development purposes.',
     version: '1.0'
   });
 });
