a38369e98c180fbdfb67a4932154821a1d933ab7
Demetrio Barrozo Dela Rosa Jr <45243271+demy2014@users.noreply.github.com>
2023-11-09 13:57:24 +0800

    Delete src/app.test.js

--- src/app.test.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.json());
-
-app.get("/test", (_req, res) =>  {
-  res.status(200).send("Hello world")
-})
-module.exports = app;
