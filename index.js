55efcb6358897cbfa3747c81ae014e8e05517224
Demetrio Barrozo Dela Rosa Jr <45243271+demy2014@users.noreply.github.com>
2023-11-09 13:06:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-const express = require("express");
-
+const express = require('express');
 const app = express();
+const port = process.env.PORT || 8080;
 
-
-app.get("/", function (req, res) {
- return res.send("Hello World via Github Actions");
+// Route for the root URL
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome to the CI Server!. Testing the CI-Dev pipeline.');
 });
 
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server is running");
+app.get('/hello', (req, res) => {
+  res.status(200).send('Hello Oklaa Dev Teams, there! This is a friendly greeting from the CI Dev-server.');
+});
+
+
+app.get('/about', (req, res) => {
+  res.status(200).json({
+    message: 'About CI-Dev Server',
+    description: 'This is a simple Express.js server for demonstration purposes.',
+    version: '1.0'
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
 module.exports = app;
